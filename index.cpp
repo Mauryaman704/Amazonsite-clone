4a458713f4522f6280eb52489bd8f1621bf797cc
Mauryaman704 <am9198566563@gmail.com>
2025-06-11 13:57:23 +0530

    Amazon-clone

---
+++ index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// int a;
+// cin >> a;
+// cout <<"value of n is " << a << endl;
+// if(a>0){
+//     cout << "a is postive" << endl;
+// }
+// else {   
+//     cout << "a is negative" << endl;
+// }
+int a,b;
+cin >> a >>b;
+cout << "value of a and b is " << a << " " << b << endl;
+
+    return 0;
+}  
